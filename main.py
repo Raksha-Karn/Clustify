736c7f0c5238a04366face101a008df95d21b443
raksha <karnraksha41@gmail.com>
2025-05-05 01:10:50 +0545

    Initialized parameters for class and centroids

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 clustered_data = np.array([[2, 10], [2, 5], [8, 4], [5, 8], [7,5], [6, 4], [1, 2], [4, 9]])
 print(clustered_data.shape)
+
+
+class KMeansClustering:
+    def __init__(self, k=3):
+        self.k = k
+        self.centroids = None
