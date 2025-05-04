eb602c1e4c7495d58a55e2351a01a9a8492e61e6
raksha <karnraksha41@gmail.com>
2025-05-05 02:47:28 +0545

    Calculated labels

--- main.py
+++ main.py
@@ -9,3 +9,18 @@ class KMeansClustering:
     def __init__(self, k=3):
         self.k = k
         self.centroids = None
+
+    @staticmethod
+    def euclidean_distance(data_point, centroids):
+        return np.sqrt(np.sum((centroids - data_point)**2, axis=1))
+
+    def fit(self, X, max_iterations=200):
+        self.centroids = np.random.uniform(np.amin(X, axis=0), np.amax(X, axis=0), size=(self.k, X.shape[1]))
+
+        for _ in range(max_iterations):
+            y = []
+            for data_point in X:
+                distances = KMeansClustering.euclidean_distance(data_point, self.centroids)
+                cluster_num = np.argmin(distances)
+                y.append(cluster_num)
+            y = np.array(y)
